ff92ca55eafcb02300538ed19a4a1330bf0f7a29
Glitch (hello-express) <none>
2021-03-24 22:08:53 +0000

    🐥🐡 Checkpoint ./server.js:16231546/1258

--- server.js
+++ server.js
@@ -3,11 +3,44 @@
 
 // https://www.freecodecamp.org/news/create-a-professional-node-express/
 
+import http from "http";
+import logger from "morgan";
+import cors from "cors";
+// routes
+import indexRouter from "./routes/index.js";
+import userRouter from "./routes/user.js";
+import chatRoomRouter from "./routes/chatRoom.js";
+import deleteRouter from "./routes/delete.js";
+// middlewares
+import { decode } from './middlewares/jwt.js'
+
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
+/** Get port from environment and store in Express. */
+const port = process.env.PORT || "3000";
+app.set("port", port);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+app.use("/users", userRouter);
+app.use("/room", decode, chatRoomRouter);
+app.use("/delete", deleteRouter);
+
+/** catch 404 and forward to error handler */
+app.use('*', (req, res) => {
+  return res.status(404).json({
+    success: false,
+    message: 'API endpoint doesnt exist'
+  })
+});
+
+/*
 // our default array of dreams
 const dreams = [
   "Find and count some sheep",
@@ -33,4 +66,13 @@ app.get("/dreams", (request, response) => {
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
+});*/
+
+/** Create HTTP server. */
+const server = http.createServer(app);
+/** Listen on provided port, on all network interfaces. */
+server.listen(port);
+/** Event listener for HTTP server "listening" event. */
+server.on("listening", () => {
+  console.log(`Listening on port:: http://localhost:${port}/`)
 });
