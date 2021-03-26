ae167d1ef69c8e501d2f7b15a251aa1691dbbf34
Glitch (hello-express) <none>
2021-03-26 23:30:34 +0000

    🐙❣️ Checkpoint ./controllers/user.js:16231546/4170 ./models/User.js:16231546/3141

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,25 @@
 // utils
-const makeValidation = require('@withvoid/make-validation');
+const makeValidation = require("@withvoid/make-validation");
 // models
-const { UserModel, USER_TYPES } = require('../models/User.js');
+const { UserModel, USER_TYPES } = require("../models/User.js");
 
 module.exports = {
-  onGetAllUsers: async (req, res) => { },
-  onGetUserById: async (req, res) => { },
+  onGetAllUsers: async (req, res) => {
+    try {
+      const users = await UserModel.getUsers();
+      return res.status(200).json({ success: true, users });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
+  onGetUserById: async (req, res) => {
+    try {
+      const user = await UserModel.getUserById(req.params.id);
+      return res.status(200).json({ success: true, user });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
   onCreateUser: async (req, res) => {
     try {
       const validation = makeValidation(types => ({
@@ -13,7 +27,7 @@ module.exports = {
         checks: {
           firstName: { type: types.string },
           lastName: { type: types.string },
-          type: { type: types.enum, options: { enum: USER_TYPES } },
+          type: { type: types.enum, options: { enum: USER_TYPES } }
         }
       }));
       if (!validation.success) return res.status(400).json(validation);
@@ -22,8 +36,18 @@ module.exports = {
       const user = await UserModel.createUser(firstName, lastName, type);
       return res.status(200).json({ success: true, user });
     } catch (error) {
-      return res.status(500).json({ success: false, error: error })
+      return res.status(500).json({ success: false, error: error });
     }
   },
-  onDeleteUserById: async (req, res) => { },
-}
+  onDeleteUserById: async (req, res) => {
+    try {
+      const user = await UserModel.deleteByUserById(req.params.id);
+      return res.status(200).json({
+        success: true,
+        message: `Deleted a count of ${user.deletedCount} user.`
+      });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  }
+};
--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const { uuidv4 } = require("uuid");
 
 const USER_TYPES = {
   CONSUMER: "consumer",
-  SUPPORT: "support",
+  SUPPORT: "support"
 };
 
 module.exports = USER_TYPES;
@@ -12,29 +12,53 @@ const userSchema = new mongoose.Schema(
   {
     _id: {
       type: String,
-      default: () => uuidv4().replace(/\-/g, ""),
+      default: () => uuidv4().replace(/\-/g, "")
     },
     firstName: String,
     lastName: String,
-    type: String,
+    type: String
   },
   {
     timestamps: true,
-    collection: "users",
+    collection: "users"
   }
 );
 
-userSchema.statics.createUser = async function (
-	firstName, 
-    	lastName, 
-    	type
-) {
+userSchema.statics.createUser = async function(firstName, lastName, type) {
   try {
     const user = await this.create({ firstName, lastName, type });
     return user;
   } catch (error) {
     throw error;
   }
-}
+};
+
+userSchema.statics.getUserById = async function(id) {
+  try {
+    const user = await this.findOne({ _id: id });
+    if (!user) throw { error: "No user with this id found" };
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+userSchema.statics.getUsers = async function() {
+  try {
+    const users = await this.find();
+    return users;
+  } catch (error) {
+    throw error;
+  }
+};
+
+userSchema.statics.deleteByUserById = async function(id) {
+  try {
+    const result = await this.remove({ _id: id });
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = mongoose.model("User", userSchema);
