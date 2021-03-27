1f83b2af4d9ba745b99d9cf5e1e73c0377168768
Glitch (hello-express) <none>
2021-03-27 00:46:03 +0000

    📍👴 Checkpoint ./middlewares/jwt.js:16231546/1442 ./routes/index.js:16231546/248

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,14 @@ const { encode } = require('../middlewares/jwt.js');
 const router = express.Router();
 
 //router
-//  .post('/login/:userId', encode, (req, res, next) => { });
+router
+  .post('/login/:userId', encode, (req, res, next) => {
+    return res
+      .status(200)
+      .json({
+        success: true,
+        authorization: req.authToken,
+      });
+  });
 
 module.exports = router;
