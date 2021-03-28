73c0eca404423dad719419c9e69f121756e42f13
Glitch (hello-express) <none>
2021-03-28 00:08:41 +0000

    🍶🙏 Checkpoint ./models/ChatMessage.js:16231546/3031 ./controllers/chatRoom.js:16231546/4919

--- controllers/chatRoom.js
+++ controllers/chatRoom.js
@@ -2,6 +2,7 @@
 const makeValidation = require("@withvoid/make-validation");
 // models
 const { ChatRoomModel, CHAT_ROOM_TYPES } = require("../models/ChatRoom.js");
+const { ChatMessageModel, CHAT_Message_TYPES } = require("../models/ChatMessage.js");
 
 module.exports = {
   initiate: async (req, res) => {
@@ -31,7 +32,32 @@ module.exports = {
       return res.status(500).json({ success: false, error: error });
     }
   },
-  postMessage: async (req, res) => {},
+  postMessage: async (req, res) => {
+    try {
+      const { roomId } = req.params;
+      const validation = makeValidation(types => ({
+        payload: req.body,
+        checks: {
+          messageText: { type: types.string }
+        }
+      }));
+      if (!validation.success) return res.status(400).json({ ...validation });
+
+      const messagePayload = {
+        messageText: req.body.messageText
+      };
+      const currentLoggedUser = req.userId;
+      const post = await ChatMessageModel.createPostInChatRoom(
+        roomId,
+        messagePayload,
+        currentLoggedUser
+      );
+      global.io.sockets.in(roomId).emit("new message", { message: post });
+      return res.status(200).json({ success: true, post });
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error });
+    }
+  },
   getRecentConversation: async (req, res) => {},
   getConversationByRoomId: async (req, res) => {},
   markConversationReadByRoomId: async (req, res) => {}
